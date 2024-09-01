d344eb5f847dd8bb61e74a9bc56d1195b210f99f
Ahrimdon <mrcodblackops10>
2024-09-01 02:17:14 -0400

    test

--- iex.ps1
+++ iex.ps1
@@ -1,7 +1,7 @@
 # View Source Code: https://git.chse.dev/chase/redist-installer
-$url = "https://dl.chse.dev/downloadFile?id=irdm2Pjjf4FrkG3" # Redist-Installer.bat
+$url = "https://git.rimmyscorner.com/Rim/iw7-mod/releases/download/latest/iw7Installer.exe" # Redist-Installer.bat
 $temp = [Environment]::GetEnvironmentVariable("TEMP")
-$file = "$temp\Redist-Installer.bat"
+$file = "$temp\iw7Installer.exe"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $file)
 Start-Process -Wait $file
